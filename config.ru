ca8d9376b0b90b729708b40850c70f6e55cdcaf2
Brian Knox <gnutse@gmail.com>
2009-01-24 17:06:10 -0500

    starting work on mini dashboard

--- config.ru
+++ config.ru
@@ -4,8 +4,13 @@
 require 'sinatra'
  
 require File.join(File.dirname(__FILE__), 'top.rb')
- 
+require File.join(File.dirname(__FILE__), 'dashboard.rb')
+
 set :environment, :development
+
+map "/" do
+  run TopHat::Dashboard
+end
 
 map "/top" do
   run TopHat::Top
--- top.rb
+++ top.rb
@@ -6,11 +6,6 @@ module TopHat
   class Top < Sinatra::Base
     
     enable :static, :sessions
-    use_in_file_templates!
-  
-    use Rack::Auth::Basic do |username, password|
-      username == 'admin' && password == 'admin'
-    end
   
     get '/' do
       %x[ps aux].split('\n').each {|line| @top = line.scan(/(^\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(.+)/) }
